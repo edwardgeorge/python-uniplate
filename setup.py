9db3c2519bed5861218f355cc2379d8ec9bbc3b1
Edward George <edwardgeorge@gmail.com>
2017-05-15 10:39:00 +0100

    enable py2 via singledispatch and bump version

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 import setuptools
+import sys
 
 setuptools.setup(
     name='uniplate',
-    version='0.0.1',
+    version='0.0.2',
     description='Python generics library using combinators from haskell\'s uniplate',
     author='Edward George',
     author_email='edwardgeorge@gmail.com',
     url='https://github.com/edwardgeorge/uniplate',
     # packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
     py_modules=['uniplate'],
+    install_requires=["singledispatch"] if sys.version_info < (3, 4) else [],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python",
     ]
 )
